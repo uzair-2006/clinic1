640d528cc7309fc2ce346fcab3c9da526e5dfadf
uzair-2006 <dodkutta@gmail.com>
2024-05-28 20:42:57 +0500

    initial

---
+++ components/headerstrip.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import Image from 'next/image';
+const Headerstrip = () => {
+    return (
+        <div className=' w-full   p-3 flex items-center justify-around py-4'>
+            <div className=' flex items-center justify-center gap-12'>
+                <div className=' flex items-center justify-start gap-2'>
+                    <Image alt='icon' height={17} width={17} src='/assets/locationIcon.png' />
+                    <p className=' text-sm  font-medium'>90919 Madie Run Apt. 790</p>
+                </div>
+                <div className=' flex items-center justify-start gap-2'>
+                    <Image alt='icon' height={20} width={20} src='/assets/messageIcon.png' />
+                    <p className=' text-sm font-medium'>hello@yoursite.com</p>
+                </div>
+            </div>
+            <div className=' flex items-center justify-start gap-2'>
+                <Image alt='icon' height={20} width={20} src='/assets/whatsappIcon.png' />
+                <p className=' text-sm font-medium'>Connect On Whatsapp</p>
+            </div>
+
+        </div>
+    );
+}
+
+export default Headerstrip;
---
+++ components/navbar.js
@@ -0,0 +1,56 @@
+"use client"
+import React from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
+import { Button } from './ui/button';
+import { usePathname } from 'next/navigation';
+
+const Navbar = () => {
+    const navLinks = [
+        { name: "Home", href: "/" },
+        { name: "About", href: "/about" },
+        { name: "Health Checkups", href: "/health" },
+        { name: "Departments", href: "/departments" },
+    ]
+    const pathname = usePathname()
+
+
+
+    return (
+        <div className=' w-full bg-white shadow-lg rounded-sm py-4 px-2 flex items-center justify-around '>
+            <div>
+                <Image alt='icon' height={110} width={110} src='/assets/logo.png' />
+            </div>
+            <div className=' flex items-center justify-center gap-20'>
+                {navLinks.map((link) => {
+                    return (
+
+                        <Link href={link.href} key={link.href}>
+                            <p className={`text-md font-medium hover:underline hover:text-[#00a0ad]  ${pathname === link.href ? 'text-[#00a0ad]' : 'text-black'} `}>{link.name}</p>
+                        </Link>
+                    )
+                })}
+
+                {/* <Link href='/'>
+                    <p className='  text-md font-medium hover:underline  hover:text-[#00a0ad]'>About</p>
+                </Link >
+                <Link href='/'>
+                    <p className='  text-md font-medium hover:underline  hover:text-[#00a0ad]'>Health Checkups</p>
+                </Link>
+                <Link href='/'>
+                    <p className=' text-md  font-medium hover:underline  hover:text-[#00a0ad]'>Departments</p>
+                </Link> */}
+
+            </div>
+            <div className=' flex items-center justify-center gap-12'>
+                <div className=' flex items-center justify-start gap-2'>
+                    <Image alt='icon' src='/assets/callIcon.png' height={17} width={17} />
+                    <p className=' font-medium'>1800-657-876</p>
+                </div>
+                <Button>Appointment</Button>
+            </div>
+        </div>
+    );
+}
+
+export default Navbar;
