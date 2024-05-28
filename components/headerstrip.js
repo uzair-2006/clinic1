c14ec5a921b9be445702b558eaf389c73dab6c32
uzair-2006 <dodkutta@gmail.com>
2024-05-28 20:51:01 +0500

    fix text color

--- components/headerstrip.js
+++ components/headerstrip.js
@@ -6,16 +6,16 @@ const Headerstrip = () => {
             <div className=' flex items-center justify-center gap-12'>
                 <div className=' flex items-center justify-start gap-2'>
                     <Image alt='icon' height={17} width={17} src='/assets/locationIcon.png' />
-                    <p className=' text-sm  font-medium'>90919 Madie Run Apt. 790</p>
+                    <p className=' text-sm  font-medium text-black'>90919 Madie Run Apt. 790</p>
                 </div>
                 <div className=' flex items-center justify-start gap-2'>
                     <Image alt='icon' height={20} width={20} src='/assets/messageIcon.png' />
-                    <p className=' text-sm font-medium'>hello@yoursite.com</p>
+                    <p className=' text-black text-sm font-medium'>hello@yoursite.com</p>
                 </div>
             </div>
             <div className=' flex items-center justify-start gap-2'>
                 <Image alt='icon' height={20} width={20} src='/assets/whatsappIcon.png' />
-                <p className=' text-sm font-medium'>Connect On Whatsapp</p>
+                <p className=' text-black text-sm font-medium'>Connect On Whatsapp</p>
             </div>
 
         </div>
